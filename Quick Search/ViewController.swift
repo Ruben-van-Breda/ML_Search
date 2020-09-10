794b5d229204bb69bf933b8bfd5f3af2da43f14f
RubenVanBreda <re.vanbreda@gmail.com>
2020-09-10 12:25:02 +0200

    Initial Commit

---
+++ Quick Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quick Search
+//
+//  Created by Ruben van Breda on 2020/09/10.
+//  Copyright © 2020 Ruben van Breda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
